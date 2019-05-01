3d3b0ce49ed764f900bfaefe9bbc9b686a19618e
BigBlueNice419 <rlewis2@andover.edu>
2019-05-01 11:04:44 -0400

    merges
    merges

--- Backend/API_proj2app/views.py
+++ Backend/API_proj2app/views.py
@@ -6,8 +6,9 @@ from django.http import HttpResponse, JsonResponse, QueryDict
 from django.urls import include, path
 from rest_framework import routers
 from API_proj2app.models import *
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 # Create your views here.
+<<<<<<< Updated upstream
 class requestHandlers(View):
     # request data will be storied in request's body
     def index(request):
@@ -70,3 +71,95 @@ class requestHandlers(View):
 
          elif request.method == "DELETE":
             id = content["id"]
+=======
+
+# request data will be storied in request's body
+def index(request):
+    return HttpResponse("welcome")
+
+def login(request):
+    # Only POST
+# from https://stackoverflow.com/questions/29780060/trying-to-parse-request-body-from-post-in-django
+
+    content = QueryDict(request.body.decode('utf-8')).dict() # content should be dict now
+    username = content["username"]
+    try:
+        user = User.objects.get(username = username)
+    except:
+        return JsonResponse({'status':'false','message':"Invalid username"}, status=406)
+    password = encrypt(content["password"])
+    if (password != user.password):
+        return JsonResponse({'status':'false','message':"Invalid password"}, status=406)
+
+    request.session["session_name"] = User.objects.get(username)#set this to the username #learned sessions from session documentation: https://docs.djangoproject.com/en/2.2/topics/http/sessions/
+    return JsonResponse({'status':'true','message':"Logged in"}, status=200)
+
+def encrypt(password):
+    return password
+
+ def newUser(request):
+     # Only POST
+     content = QueryDict(request.body.decode('utf-8')).dict()
+     lastname = content["lastname"]
+     firstname = content["firstname"]
+     username = content ["username"]
+     password = conetnt ["password"]
+
+     # data = {}
+     # data = QueryDict(request.META["QUERY_STRING"]).dict()
+
+     try:
+         new_user = User.objects.create(firstname = firstname, lastname = lastname, username = username, password = password)
+     except:
+         return JsonResponse({'status':'false','message':"Invalid username or password"}, status=406)
+
+     if request.method == "POST":
+
+         for user in User.objects.filter(username = user.get(username)):
+             if username == user.get(username):
+                 return JsonResponse({'status':'false','message':"This username is already in use"}, status=406)
+         new_user.save()
+    return JsonResponse({'status':'true','message':"Your account has been created, please login"}, status=200)
+     # NEED CODE HERE TO ACCESS FIRSTNAME, LASTNAME, USERNAME, and PASSWORD from request
+
+
+# handle all requests at memories
+def handleMemories(request):
+    session_name = request.session["session_name"]
+
+    content = QueryDict(request.body.decode('utf-8')).dict()
+
+    if request.method == "GET":
+        data = {}
+
+        for memory in Memory.objects.filter(username = session_name):
+            data[memory.id] = {"title": memory.title, "content": memory.content, "image": memory.image, "date": memory.date}
+
+        return JsonResponse(data, status=200)
+
+     elif request.method == "POST":
+        title = content["title"]
+        photo = content["photo"]
+        text = content["text"]
+        date = content["date"]
+
+        Memory.object.create(title = title, image = photo, content = text, date = date)
+        return JsonResponse(data, status=200)
+
+     elif request.method == "PATCH":
+         title = content["title"]
+         photo = content["photo"]
+         text = content["text"]
+         date = content["date"]
+         id = content["id"]
+
+         #still working on it
+
+     elif request.method == "DELETE":
+        id = content["id"]
+
+        for memory in Memory.objects.filter(username = session_name):
+            if memory.id == id:
+                memory.delete()
+                return JsonResponse({"Message":"DELETED (200)"})
+>>>>>>> Stashed changes
